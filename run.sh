6995c017ac716f4f2eadcd46404a1734369179e2
Tyler Baker <tyler@foundries.io>
2019-01-03 00:21:23 -0800

    Initial commit
    Signed-off-by: Tyler Baker <tyler@foundries.io>

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker pull fullnodes/lighting:latest
+docker stop lightningd
+docker rm lightningd
+docker volume create lightning
+docker run -dit --name lightningd --net=host --restart=always -v lighting:/root/.lightning fullnodes/lightning:latest --bitcoin-rpcconnect=127.0.0.1 --bitcoin-rpcuser=admin --bitcoin-rpcpassword=changeme --network=bitcoin --alias=fullnodes --log-level=debug
+mkdir -p ~/.bin
+cp $pwd/bin/lightning-cli ~/.bin
