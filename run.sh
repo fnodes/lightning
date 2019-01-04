5cd57d2a8314bfced535e8828e709b0564fd8336
Tyler Baker <tyler@foundries.io>
2019-01-03 19:19:27 -0800

    Add lightningd helper
    Signed-off-by: Tyler Baker <tyler@foundries.io>

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ docker rm lightningd
 docker volume create lightning
 docker run -dit --name lightningd --net=host --restart=always -v lighting:/root/.lightning fullnodes/lightning:latest --bitcoin-rpcconnect=127.0.0.1 --bitcoin-rpcuser=admin --bitcoin-rpcpassword=changeme --network=bitcoin --alias=fullnodes --log-level=debug
 mkdir -p ~/.bin
-cp $pwd/bin/lightning-cli ~/.bin
+cp $(pwd)/bin/lightning* ~/.bin
